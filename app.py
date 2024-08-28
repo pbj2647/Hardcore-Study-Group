fc7c10e454bb897241c45545f9b0fc8476561dea
Park ByoungJun <pbj2647_@naver.com>
2024-08-28 17:45:52 +0900

    1st 회원가입, 로그인, 게시판 페이지 나누기
    페이지 나누기 및 db 관련 공부
    => 저녁에 게시판 출력, 게시판 작성 만들기

--- app.py
+++ app.py
@@ -1,6 +1,61 @@
-from flask import Flask, render_template
+from flask import Flask, request, redirect, url_for, render_template
+import pymysql
+
 app = Flask(__name__)
 
+def connectdb():
+    conn = pymysql.connect(host='localhost',
+                     user='root',
+                     passwd='root',
+                     db='hardcoreDB',
+                     charset='utf8'
+                     )
+    return conn 
+
 @app.route('/')
+def main():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template('login.html')
+    if request.method =="POST":
+        username = request.form['username']
+        passwd = request.form['passwd']
+        return render_template('board.html')
+    else:
+        return render_template('login.html')
+
+@app.route('/board')
+def viewBoard():
+    return render_template('board.html')
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method =="POST":
+        username = request.form['username']
+        passwd = request.form['passwd']
+        conn = connectdb()
+        cursor = conn.cursor()
+        query = f"insert into usertable (username, passwd) values ('{username}', '{passwd}');"
+        cursor.execute(query)
+        conn.commit()
+        conn.close()
+        return render_template('login.html')
+    else:
+        return render_template('signup.html')
+    
+@app.route('/board', methods=['GET'])
+def viewboard():
+    conn = connectdb()
+    cursor = conn.cursor()
+    query = "select * from boardtable"
+    cursor.execute(query)
+    board_data = cursor.fetchall
+    conn.commit()
+    conn.close()
+    return render_template('board.html', board_data=board_data)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- run.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask, render_template
-run = Flask(__name__)
-
-if __name__ == '__main__':
-    run.run(debug=True)
